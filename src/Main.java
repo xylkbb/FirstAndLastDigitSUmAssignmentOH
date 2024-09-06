8e55ec75481edc907ac6ea287d25882d06229c52
Eric Coe <coeeri001@warren.k12.in.us>
2024-09-06 12:57:19 -0400

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+    }
+
+    private static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int firstDigit = 0;
+        int lastDigit = number % 10;
+
+        while (number > 0){
+            firstDigit = number;
+            number /= 10;
+        }
+
+        return firstDigit + lastDigit;
+
+    }
 }
